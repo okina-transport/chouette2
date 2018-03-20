c2124e6be5a34f2b4589a01fb403c9be98cd434f
Mathieu Hicauber <mhicauber@okina.fr>
2018-03-20 09:03:12 +0100

    testing removing question marks

--- db/migrate/20150312104557_set_default_value_for_data_format_in_organisation.rb
+++ db/migrate/20150312104557_set_default_value_for_data_format_in_organisation.rb
@@ -1,18 +1,18 @@
 class SetDefaultValueForDataFormatInOrganisation < ActiveRecord::Migration
   def change
     Organisation.all.each do |organisation|
-      if organisation.data_format.neptune?
+      if organisation.data_format == "neptune"
         organisation.update_attributes :data_format => "neptune"
       end
     end
     Referential.all.each do |referential|
-      if referential.data_format.neptune?
+      if referential.data_format == "neptune"
         referential.update_attributes :data_format => "neptune"
-      elsif referential.data_format.netex?
+      elsif referential.data_format == "netex"
         referential.update_attributes :data_format => "netex"
-      elsif referential.data_format.gtfs?
+      elsif referential.data_format == "gtfs"
         referential.update_attributes :data_format => "gtfs"
-      elsif referential.data_format.hub?
+      elsif referential.data_format == "hub"
         referential.update_attributes :data_format => "hub"
       end
     end
