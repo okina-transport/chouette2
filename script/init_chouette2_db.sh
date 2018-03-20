27b3131b545597cf4f04b40a00c765a87a58f3b2
Mathieu Hicauber <mhicauber@okina.fr>
2018-03-20 12:47:29 +0100

    added start script for docker-compose

--- script/init_chouette2_db.sh
+++ script/init_chouette2_db.sh
@@ -3,5 +3,4 @@
 RAILS_ENV=production bundle exec rake db:gis:setup
 RAILS_ENV=production bundle exec rake db:migrate
 RAILS_ENV=production bundle exec rake assets:clobber assets:precompile
-RAILS_ENV=production bundle exec rails server -b 0.0.0.0
 
