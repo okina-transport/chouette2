5ef7b18c4af88c2172fc58fa99c1a55813c0ee43
Mathieu Hicauber <mhicauber@okina.fr>
2018-03-21 12:06:04 +0100

    update startup script renamed config.serve_static_files reference

--- script/init_db_and_start_server.sh
+++ script/init_db_and_start_server.sh
@@ -2,7 +2,6 @@
 
 RAILS_ENV=production bundle exec rake db:gis:setup
 RAILS_ENV=production bundle exec rake db:migrate
-RAILS_ENV=production bundle exec rake assets:clobber assets:precompile
 
 RAILS_ENV=production bundle exec rake jobs:work
 
