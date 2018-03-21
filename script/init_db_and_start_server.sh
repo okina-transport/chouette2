fcf9688fc095d26272ed57450d4195dd14abf43a
Mathieu Hicauber <mhicauber@okina.fr>
2018-03-21 12:17:04 +0100

    removed failing rake task

--- script/init_db_and_start_server.sh
+++ script/init_db_and_start_server.sh
@@ -3,6 +3,4 @@
 RAILS_ENV=production bundle exec rake db:gis:setup
 RAILS_ENV=production bundle exec rake db:migrate
 
-RAILS_ENV=production bundle exec rake jobs:work
-
 RAILS_ENV=production bundle exec rails server
