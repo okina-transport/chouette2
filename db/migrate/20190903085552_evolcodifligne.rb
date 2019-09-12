456d922cf68d7871fbaea42bd09de0c6236c312a
schabanne <schabanne@okina.fr>
2019-09-12 17:11:03 +0200

    fix db

--- db/migrate/20190903085552_evolcodifligne.rb
+++ db/migrate/20190903085552_evolcodifligne.rb
@@ -1,7 +1,5 @@
 class Evolcodifligne < ActiveRecord::Migration
-class Evolcodifligne < ActiveRecord::Migration
   def change
     add_column :lines, :codifligneCommercial, :string, :limit => 50
   end
 end
-c
